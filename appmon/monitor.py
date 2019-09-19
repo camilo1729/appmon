939d72aac65a193238b109de4938348f6689c0ca
cristian ruiz <camilo1729@gmail.com>
2019-09-19 22:34:40 +0200

    Adapt monitor main program

--- appmon/monitor.py
+++ appmon/monitor.py
@@ -1,29 +1,13 @@
 import collector.net as net
 import time
 
-
-
-netcollect = net.Net()
-
 previous = None
-time_freq = 10
+time_freq = 5
 
+#   for field,value in group.iteritems():
+#     print('{} : {} Mbytes/sec'.format(field,(value[1]-value[0]))))
 
-def delta(data=None,field='', group_by=''):
-  group={}
-  for register in data:
-    if not register[group_by] in group:
-      group[register[group_by]]=[]
-
-    group[register[group_by]].append(float(register[field]))
-
-  for field,value in group.iteritems():
-    print('{} : {}'.format(field,value[1]-value[0]))
-
-
-previous = netcollect.execute()
+netcollect = net.Net(command='/sbin/ifconfig wlp3s0')
 while True:
   time.sleep(time_freq)
-  current=netcollect.execute()
-  delta(previous+current,'bytes','type')
-  previoius=current
+  print(netcollect.get_delta())
